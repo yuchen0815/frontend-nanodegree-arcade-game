561002b5add33d1c64c1aaf00ec2981ad557881f
lychen <2956616387@qq.com>
2017-09-19 10:32:06 +0800

    Update app.js
    对一些错误进行修正

--- js/app.js
+++ js/app.js
@@ -1,10 +1,15 @@
+//定义一些魔法数字
+var HANDLE_WIDTH = 101;
+var HANDLE_HEIGHT = 83;
+var PLAYER_WIDTH = 200;
+var PLAYER_HEIGHT = 410;
 // 这是我们的玩家要躲避的敌人 
 var Enemy = function(coordinate) {
     // 这要应用到每个敌人的实例的变量
     this.x = -50;
     this.y = coordinate;
-    this.width = 83;
-    this.height = 101;
+    this.width = HANDLE_HEIGHT;
+    this.height = HANDLE_WIDTH;
     this.speed = Math.random()*250 + 150;
     this.sprite = 'images/enemy-bug.png';
 };
@@ -13,23 +18,24 @@ Enemy.prototype.update = function(dt) {
     if(this.x <= 505){
         this.x +=this.speed * dt;//表示以相同速度进行
     }else{
-        this.x = -50;        
+        this.x = -HANDLE_WIDTH;        
     }
+    this.checkForCollision();
 };
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 Enemy.prototype.checkForCollision = function(){
-    if(this.x < (player.x + player.width) && (this.x + this.width) > player.x && this.y < (player.y + player.heigh) && (this.y + this.height) > player.y){
+    if(this.x < (player.x + player.width) && (this.x + this.width) > player.x && this.y < (player.y + player.height) && (this.y + this.height) > player.y){
         alert("oh!no!Game over!");
         player.resetPlayer();
     }
 }
 var Player = function(){
-    this.x = 200;
-    this.y = 410;
-    this.width = 83;
-    this.height = 83;
+    this.x = PLAYER_WIDTH;
+    this.y = PLAYER_HEIGHT;
+    this.width = HANDLE_HEIGHT;
+    this.height = HANDLE_HEIGHT;
     this.sprite ='images/char-princess-girl.png'; 
 }
 Player.prototype.update = function(){
@@ -40,34 +46,34 @@ Player.prototype.update = function(){
 
 }
 Player.prototype.resetPlayer = function(){
-    this.x = 200;
-    this.y = 410;
+    this.x = PLAYER_WIDTH;
+    this.y = PLAYER_HEIGHT;
 }
 Player.prototype.handleInput = function(allowedKeys){
     switch(allowedKeys){
         case 'left':
             if(this.x >= 0){ 
-                this.x -= 101;
-            }; break;
+                this.x -= HANDLE_WIDTH;
+            }break;
         case 'right': 
             if(this.x <= 303){
-                this.x +=100; 
-            };break;
+                this.x += HANDLE_WIDTH; 
+            }break;
         case 'up': 
             if(this.y >= 55){    
-                this.y -=83; 
-            };break;
+                this.y -= HANDLE_HEIGHT; 
+            }break;
         case 'down':
             if(this.y <= 404){
-                this.y +=83; 
-            };break;
+                this.y += HANDLE_HEIGHT; 
+            }break;
     }
 };
 Player.prototype.render = function(){
     ctx.drawImage(Resources.get(this.sprite),this.x,this.y);
 }
 
-var allEnemies = [new Enemy(101),new Enemy(200),new Enemy(220)];
+var allEnemies = [new Enemy(HANDLE_WIDTH),new Enemy(PLAYER_WIDTH),new Enemy(220)];
 var player = new Player();
 
 document.addEventListener('keyup', function(e) {
